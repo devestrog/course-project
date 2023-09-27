97908abc70be1b1a3c1d3ac6ee8ed7d92d9f87b2
devestrog <devestrog@gmail.com>
2023-09-27 21:41:44 +0300

    85 lecture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
